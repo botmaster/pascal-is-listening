9c136817ccdb0c91b41494999d7bfc58e7f7c6fa
botmaster <pascal.achard@gmail.com>
2019-10-09 22:38:18 +0200

    Fix auth

--- pages/auth.vue
+++ pages/auth.vue
@@ -35,7 +35,7 @@ export default {
     mounted() {
         console.log(this.query.success, this.query.error, this.isConnected)
         if (!(this.query.success || this.query.error) && !this.isConnected) {
-            // window.location = this.spotifyUrl
+            window.location = this.spotifyUrl
         } else if (Object.keys(this.query).length !== 0) {
             window.history.replaceState(
                 {},
