c569e5dce5c2816cbd08cee5eba304a9da587da6
botmaster <pascal.achard@gmail.com>
2019-10-10 22:34:46 +0200

    Update head tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,13 @@ export default {
      ** Headers of the page
      */
     head: {
+        // this htmlAttrs you need
+        htmlAttrs: {
+            lang: 'fr-fr'
+        },
+        bodyAttrs: {
+            /* id: 'js-scroll' */
+        },
         title: process.env.npm_package_name || '',
         meta: [
             { charset: 'utf-8' },
